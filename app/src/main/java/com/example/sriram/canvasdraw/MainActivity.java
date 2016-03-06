0a923c423ae284a3d8d9592923666d0096c075dd
sriram <salagappa@provocraft.com>
2016-03-06 11:16:51 -0700

    android canvas draw

---
+++ app/src/main/java/com/example/sriram/canvasdraw/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.sriram.canvasdraw;
+
+import android.graphics.Path;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
+
+
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Bitmap bg = Bitmap.createBitmap(480, 800, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bg);
+        //canvas.drawRect(50, 50, 200, 200, paint);
+        Paint paint = new Paint();
+        Path path = new Path();
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.TRANSPARENT);
+        canvas.drawPaint(paint);
+        for (int i = 50; i < 100; i++) {
+            path.moveTo(i, i-1);
+            path.lineTo(i, i);
+        }
+        path.close();
+        paint.setStrokeWidth(3);
+        paint.setPathEffect(null);
+        paint.setColor(Color.BLACK);
+        paint.setStyle(Paint.Style.STROKE);
+        canvas.drawPath(path, paint);
+
+        LinearLayout ll = (LinearLayout) findViewById(R.id.rect);
+        ll.setBackground(new BitmapDrawable(bg));
+    }
+
+}
